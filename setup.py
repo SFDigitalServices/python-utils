ecfa0034c8250ebdc0e3313cd42f6b1481d12665
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-10-04 13:31:38 -0700

    update to new repo name

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='python-utils',
-    url='https://github.com/SFDigitalServices/python-utils',
+    url='https://github.com/SFDigitalServices/sfds-python-utils',
     author='Josh Chou',
     author_email='joshua.chou@sfgov.org ',
-    packages=['python-utils'],
+    packages=['sfds-python-utils'],
     install_requires=['falcon'],
     version='0.1',
     license='MIT',
