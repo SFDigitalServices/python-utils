1b4e9f2beb059c846e0a5ae20897d42a8f24bd0a
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-10-04 13:09:10 -0700

    initial checkin

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-utils',
+    url='https://github.com/SFDigitalServices/python-utils',
+    author='Josh Chou',
+    author_email='joshua.chou@sfgov.org ',
+    packages=['python-utils'],
+    install_requires=['falcon', 'os'],
+    version='0.1',
+    license='MIT',
+    description='Useful(?) python code',
+)
